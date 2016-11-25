89072229a6189b1c07ab7060755ffa98e20ba8fd
Admir Alicic <admir_alicic@yahoo.com>
2016-11-25 11:02:44 +0100

    database connect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  var products = Product.find(function (err, products) {
+    var productChunks = [];
+    var chunkSize = 3;
+    for (var i = 0; i < products.length; i += chunkSize){
+      productChunks.push(products.slice(i, i + chunkSize));
+    }
+    console.log(productChunks);
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+  });
 });
 
 module.exports = router;
